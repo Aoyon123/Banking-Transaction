b8827473ffb74193a2aadbf9532bb6ca5cd51601
Aoyon123 <49103044+Aoyon123@users.noreply.github.com>
2021-12-04 23:04:35 +0600

    Functionality start

--- script.js
+++ script.js
@@ -61,16 +61,93 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  movements.forEach(function (mov, i) {
+    const html = `
+  <div class="movements__row">
+    <div class="movements__type
+    movements__type--deposit">${i + 1}</div>
+    <div class="movements__value">${mov}</div>
+  </div>  `;
+  });
+};
+displayMovements(account1, movements);
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+
+/*
+
+//Simple Array Methods
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+//SLICE
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-1));
+console.log(arr.slice());
+console.log([...arr]);
+
+//SPLICE
+console.log(arr.splice(1, 2));
+console.log(arr.splice(2));
+console.log(arr);
+arr.splice(-1);
+console.log(arr);
+
+//REVERSE
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+
+//CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+
+//JOIN
+console.log(letters.join(' - '));
+
+
+*/
+
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const movement of movements) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdraw ${Math.abs(movement)}`);
+  }
+}
+
+console.log('      ')
+console.log('----FOREACH----');
+movements.forEach(function (movement) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You Withdraw ${Math.abs(movement)}`);
+  }
+});
+
+*/
+/*
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+currencies.forEach(function (value, key, map) {
+  console.log(`${key} : ${value}`);
+});
 
-/////////////////////////////////////////////////
+*/
